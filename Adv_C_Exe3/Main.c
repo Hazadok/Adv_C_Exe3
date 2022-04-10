c1b2f1dede1f224de5604c4350b68df84526a69e
matatheking <matatheking@gmail.com>
2022-04-10 23:11:05 +0300

    first part of stack.c is completed

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Stack.h"
 #include "Queue.h"
 
+//typedef struct charNode {
+//	char data;
+//	struct charNode* next;
+//} charNode;
+//
+//
+//typedef struct Stack {
+//	charNode* head;
+//} Stack;
+
+
 void main()
 {
-	// add your code here
+	charNode* node = (charNode*)malloc(1 * sizeof(charNode));
+	if (node==NULL)
+	{
+		exit(1);
+	}
+	Stack* s = (Stack*)malloc(1*sizeof(Stack));
+	if (s==NULL)
+	{
+		exit(1);
+	}
+	
+	initStack(s);
+	int is_empty = isEmptyStack(s);
+	printf("%d\n", is_empty);
+	
+	
+	char data = 'a';
+	char data2 = 'b';
+
+	
+	push(s, data);
+	printf("%c\n", s->head->data);
+	push(s, data2);
+	printf("%c\n", s->head->data);
+
+	pop(s);
+	printf("%c\n", s->head->data);
+    
+	int is_stil_empty = isEmptyStack(s);
+	printf("%d\n", is_stil_empty);
+
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,37 +1,84 @@
 #include "Stack.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	charNode* scouter = NULL;
+	charNode* current = s->head;
+
+	while (current)
+	{
+		scouter = current->next;
+		free(current);
+		current = scouter;
+	}
+	
+	s->head = NULL;
+		
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* new_node = (charNode*)calloc(1, sizeof(charNode));
+	if (new_node==NULL)
+	{
+		exit(1);
+	}
+
+
+	new_node->data = data;
+	
+	if (s->head == NULL)
+	    s->head = new_node;
+
+	
+	else
+	
+	{
+		new_node->next = s->head;
+		s->head = new_node;
+	}
 }
+
+
+
 
 char pop(Stack* s)
 {
-	// add your code here
+	char ex = s->head->data;
+	charNode* current = s->head;
+	s->head = s->head->next;
+	free(current);
+
+	return ex;
+	
 }
+
+
+
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (s->head == NULL)
+		return 1;
+
+	else return 0;
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
 
 void flipBetweenHashes(const char* sentence)
 {
+
 	// add your code here
 }
 
